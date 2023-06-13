55a0a3318e177c316be6d1143a8115ab2db1b64e
saeed <saeedblack77@gmail.com>
2023-06-13 19:04:19 +0330

    khooneh 1402/03/23 19:04

--- app.vue
+++ app.vue
@@ -29,7 +29,14 @@
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
+          <TransitionChild as="template"
+                           enter="transition ease-in-out duration-300 transform"
+                           enter-from="translate-x-full"
+                           enter-to="translate-x-0"
+                           leave="transition ease-in-out duration-300 transform"
+                           leave-from="translate-x-0"
+                           leave-to="translate-x-full"
+          >
             <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
               <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
