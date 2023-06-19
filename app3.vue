17fb59e619cd119960ff8c2350aaadc4af9e6c94
saeed javadzadeh <saeedblack77@gmail.com>
2023-06-19 18:23:47 +0330

    laptop 1402/03/29 18:12

---
+++ app3.vue
@@ -0,0 +1,136 @@
+<!--
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<template>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-white">
+    <body class="h-full">
+    ```
+  -->
+  <div>
+    <TransitionRoot as="template" :show="sidebarOpen">
+      <Dialog as="div" class="relative z-50 lg:hidden" @close="sidebarOpen = false">
+        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+                         enter-to="opacity-100" leave="transition-opacity ease-linear duration-300"
+                         leave-from="opacity-100" leave-to="opacity-0">
+          <div class="fixed inset-0 bg-gray-900/80"/>
+        </TransitionChild>
+
+        <div class="fixed inset-0 flex">
+          <TransitionChild as="template"
+                           enter="transition ease-in-out duration-300 transform"
+                           enter-from="translate-x-full"
+                           enter-to="translate-x-0"
+                           leave="transition ease-in-out duration-300 transform"
+                           leave-from="translate-x-0"
+                           leave-to="translate-x-full"
+          >
+            <DialogPanel class="relative ml-0 flex w-full max-w-xs flex-1">
+              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                               enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100"
+                               leave-to="opacity-0">
+                <div class="absolute right-full top-0 flex w-16 justify-center pt-5">
+                  <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                    <span class="sr-only">Close sidebar</span>
+                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true"/>
+                  </button>
+                </div>
+              </TransitionChild>
+              <!-- Sidebar component, swap this element with another sidebar if you like -->
+              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
+                <!-- Modal content -->
+                <p>dgdgdgdg</p>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 ">
+      <!-- Sidebar content -->
+      <p>dgdgdgdg</p>
+    </div>
+
+    <div class="lg:pr-72">
+      <div
+          class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+        <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
+          <span class="sr-only">Open sidebar</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true"/>
+        </button>
+        <!-- Navbar items -->
+        <p>dgdgdgdg</p>
+      </div>
+
+      <main class="bg-gray-200 py-10">
+        <div class="px-4 sm:px-6 lg:px-8">
+          <!-- Your content -->
+          <NuxtPage/>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import {
+  Dialog,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
+import {
+  Bars3Icon,
+  BellIcon,
+  CalendarIcon,
+  ChartPieIcon,
+  Cog6ToothIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from '@heroicons/vue/24/outline'
+import {ChevronDownIcon, MagnifyingGlassIcon} from '@heroicons/vue/20/solid'
+
+const navigation = [
+  {name: 'Dashboard', href: '#', icon: HomeIcon, current: true},
+  {name: 'Team', href: '#', icon: UsersIcon, current: false},
+  {name: 'Projects', href: '#', icon: FolderIcon, current: false},
+  {name: 'Calendar', href: '#', icon: CalendarIcon, current: false},
+  {name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false},
+  {name: 'Reports', href: '#', icon: ChartPieIcon, current: false},
+]
+const teams = [
+  {id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false},
+  {id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false},
+  {id: 3, name: 'Workcation', href: '#', initial: 'W', current: false},
+]
+const userNavigation = [
+  {name: 'Your profile', href: '#'},
+  {name: 'Sign out', href: '#'},
+]
+
+const sidebarOpen = ref(false)
+</script>
