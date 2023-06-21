3f97da153583b20a7d0e98fa0f7782c2f23ccf8b
saeed <saeedblack77@gmail.com>
2023-06-21 23:18:22 +0330

    khooneh 1402/03/31 23:18

---
+++ components/Main/Part.vue
@@ -0,0 +1,11 @@
+<!-- Main part of page -->
+<template>
+  <div class="lg:pr-72">
+    <main class="bg-gray-200 py-10">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        <NuxtPage/>
+      </div>
+    </main>
+  </div>
+</template>
