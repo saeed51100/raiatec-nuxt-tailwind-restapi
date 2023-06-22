6a235d83cf07eb579ff17cb04fcafff0a0f67a80
saeed <saeedblack77@gmail.com>
2023-06-22 22:35:13 +0330

    khooneh 1402/04/01 22:32

--- components/Sidebar/Part.vue
+++ components/Sidebar/Part.vue
@@ -3,7 +3,7 @@
   <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex mt-16 lg:w-72 lg:flex-col-reverse">
     <!-- Sidebar component, swap this element with another sidebar if you like -->
     <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-red-300 px-6 pb-4 pt-5">
-      <Part />
+      <Part/>
     </div>
   </div>
 </template>
