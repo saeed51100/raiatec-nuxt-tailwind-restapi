2fc1365852ef60602c5465bafc197226883551d0
saeed <saeedblack77@gmail.com>
2023-06-23 09:41:35 +0330

    khooneh 1402/04/02 9:41

--- components/Sidebar/SidebarPart.vue
+++ components/Sidebar/SidebarPart.vue
@@ -3,18 +3,8 @@
   <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex mt-16 lg:w-72 lg:flex-col-reverse">
     <!-- Sidebar component, swap this element with another sidebar if you like -->
     <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-red-300 px-6 pb-4 pt-5">
-      <Part/>
+      <ListOfPostsPart />
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import Part from '@/components/ListOfPosts/ListOfPostsPart.vue'
-
-export default {
-  components: {
-    Part,
-  },
-  // MyComponent's logic
-}
-</script>
