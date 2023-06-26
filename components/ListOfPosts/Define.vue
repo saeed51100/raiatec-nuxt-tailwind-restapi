1018edfc4a451694dc481b76dbeed242242ac265
saeed <saeedblack77@gmail.com>
2023-06-26 08:17:08 +0330

    edare 1402/04/05 8:16

---
+++ components/ListOfPosts/Define.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  slug?: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink v-if="slug" :to="`/${slug}`">{{ title }}</NuxtLink>
+</template>
