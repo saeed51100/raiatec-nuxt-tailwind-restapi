c1b65db97b97aaa96eab4a2bea50643df5546951
saeed <saeedblack77@gmail.com>
2023-07-02 13:06:31 +0330

    edare 1402/04/11 7:46

--- components/ListOfPosts/index.vue
+++ components/ListOfPosts/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="post in nonCategorizedPosts" :key="post.id" :style="{ marginTop: '5px', listStyleType: 'none' }">
+      <li v-for="post in nonCategorizedPosts" :key="post.id" >
         <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
           {{ post.title.rendered }}
         </nuxt-link>
@@ -9,8 +9,8 @@
     </ul>
 
     <div v-for="category in categories" :key="category.id">
-      <h5 class="cat-title bg-info">{{ category.name }}</h5>
-      <div class="cat-content">
+      <h5 class="bg-red-200">{{ category.name }}</h5>
+      <div >
         <ul>
           <li v-for="post in category.posts" :key="post.id" :style="{ marginTop: '5px', listStyleType: 'none' }">
             <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
@@ -24,11 +24,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import {ref, computed} from 'vue';
 import useWpApi from '~/composables/useWpApi';
 
-const { data: posts } = await useWpApi().getPosts();
-const { data: categories } = await useWpApi().getCatgories();
+const {data: posts} = await useWpApi().getPosts();
+const {data: categories} = await useWpApi().getCatgories();
 
 const currentId = ref(null); // Set the current ID
 const isSingle = ref(false); // Set the single flag
