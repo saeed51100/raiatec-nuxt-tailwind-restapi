2874a2d7fb0cddf99b6a1bdc260af1d21d23d64e
saeed <saeedblack77@gmail.com>
2023-07-06 09:29:22 +0330

    khooneh 1402/04/14 22:18

--- components/ListOfPosts/old-files/index1.vue
+++ components/ListOfPosts/old-files/index1.vue
@@ -1,7 +1,8 @@
+<!-- final from chatgpt4 -->
 <template>
   <div>
     <ul>
-      <li v-for="post in nonCategorizedPosts" :key="post.id" >
+      <li v-for="post in nonCategorizedPosts" :key="post.id" :style="{ marginTop: '5px', listStyleType: 'none' }">
         <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
           {{ post.title.rendered }}
         </nuxt-link>
@@ -9,8 +10,8 @@
     </ul>
 
     <div v-for="category in categories" :key="category.id">
-      <h5 class="bg-red-200">{{ category.name }}</h5>
-      <div >
+      <h5 class="cat-title bg-info">{{ category.name }}</h5>
+      <div class="cat-content">
         <ul>
           <li v-for="post in category.posts" :key="post.id" :style="{ marginTop: '5px', listStyleType: 'none' }">
             <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
@@ -24,11 +25,11 @@
 </template>
 
 <script setup lang="ts">
-import {ref, computed} from 'vue';
+import { ref, computed } from 'vue';
 import useWpApi from '~/composables/useWpApi';
 
-const {data: posts} = await useWpApi().getPosts();
-const {data: categories} = await useWpApi().getCatgories();
+const { data: posts } = await useWpApi().getPosts();
+const { data: categories } = await useWpApi().getCatgories();
 
 const currentId = ref(null); // Set the current ID
 const isSingle = ref(false); // Set the single flag
