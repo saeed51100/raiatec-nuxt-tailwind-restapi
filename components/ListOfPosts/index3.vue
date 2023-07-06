a56f02fcec0927ea59392a3bd6d501978fb002c3
saeed <saeedblack77@gmail.com>
2023-07-06 09:06:53 +0330

    khooneh 1402/04/14 22:18

--- components/ListOfPosts/index3.vue
+++ components/ListOfPosts/index3.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <ul>
-      <li v-for="post in nonCategorizedPosts" :key="post.id" >
-        <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
-          {{ post.title.rendered }}
-        </nuxt-link>
-      </li>
-    </ul>
 
-    <div v-for="category in categories" :key="category.id">
-      <h5 class="bg-red-200">{{ category.name }}</h5>
-      <div >
-        <ul>
-          <li v-for="post in category.posts" :key="post.id" :style="{ marginTop: '5px', listStyleType: 'none' }">
-            <nuxt-link :to="post.link" :class="{ 'list-group': post.id === currentId && isSingle }">
-              {{ post.title.rendered }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </div>
+    <div v-for="post in posts " :key="post.id">
+      <!-- show post.title if exists in concatenatedTitles else execute new logic -->
+      <template v-if="concatenatedTitles.includes(post.title.rendered)">
+        {{ post.title.rendered }}
+      </template>
+      <template v-else>
+        <!-- categorized posts -->
+        <div v-for="category in categories" :key="category.id">
+          <template v-if="post.categories.includes(category.id)">
+            <h3 class="bg-red-200">{{ category.name }}</h3>
+
+            <!-- Store category.name to array -->
+            concatenatedCategory.push(category.name)
+            <ul>
+              <li v-for="relatedPost in category.posts" :key="relatedPost.id">
+                {{ relatedPost.title.rendered }}
+              </li>
+            </ul>
+          </template>
+        </div>
+      </template>
     </div>
+
   </div>
 </template>
 
@@ -30,9 +34,6 @@ import useWpApi from '~/composables/useWpApi';
 const {data: posts} = await useWpApi().getPosts();
 const {data: categories} = await useWpApi().getCatgories();
 
-const currentId = ref(null); // Set the current ID
-const isSingle = ref(false); // Set the single flag
-
 const nonCategorizedPosts = computed(() => {
   return posts.value.filter((post) => !post.categories.length);
 });
@@ -42,4 +43,14 @@ categories.value.forEach((category) => {
       post.categories.includes(category.id)
   );
 });
+
+// Initialize a variable to store concatenated titles
+const concatenatedTitles = ref([]);
+const concatenatedCategory = ref([]);
+
+// Concatenate post titles and store them in the 'concatenatedTitles' variable
+nonCategorizedPosts.value.forEach((post) => {
+  concatenatedTitles.value += post.title.rendered;
+});
+
 </script>
