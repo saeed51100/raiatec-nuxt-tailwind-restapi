c7316ae06aa70fc8ebdbb180d1ddc3f97d9595be
saeed <saeedblack77@gmail.com>
2023-07-11 20:55:19 +0330

    khooneh

--- components/ListOfPosts/index.vue
+++ components/ListOfPosts/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>
-    {{ post.title.rendered }}
-  </h1>
+
+  <div v-for="category in categories" :key="(category ).id" class="bg-red-200">
+    {{ (category).name }}
+  </div>
+
+  <div v-for="post in posts " :key="post.id">
+    <NuxtLink v-if="post.slug" :to="`/${post.slug}`" @click="$emit('close-modal')">{{ post.title.rendered }}</NuxtLink>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 const {data: posts, error} = await useWpApi().getPosts();
+const {data: categories} = await useWpApi().getCatgories();
 </script>
